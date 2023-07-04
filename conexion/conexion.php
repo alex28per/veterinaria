57c498131a55d496b3fea8d1acb319bec4c52e5d
alex28per <137488715+alex28per@users.noreply.github.com>
2023-07-03 22:53:00 -0600

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,1 +1,15 @@
+<?php
+/*conexion a la base de datos veterinaria*/
+$servidor = "localhost";
+$usuario = "root";
+$contraseña = "";
+$basedatos = "veterinaria";
+
+/*obtiene la conexion fisica a la bd */
+$cn = mysqli_connect($servidor,$usuario,$contraseña);
+
+/*selecciona la bd*/
+mysqli_select_db($cn,$basedatos);
+
+?>
 
