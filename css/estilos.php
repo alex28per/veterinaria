c084ca9791d739b0e07735fb0fb2b0fc29847b2d
alex28per <137488715+alex28per@users.noreply.github.com>
2023-07-03 22:54:08 -0600

    Update estilos.php

--- css/estilos.php
+++ css/estilos.php
@@ -1,1 +1,52 @@
+<?php
+
+header("Content-Type: text/css; charset:UTF-8");
+?>
+
+.bg-file{
+    background-color:#33B5FF;
+}
+
+.atr{
+    color:#FFF;
+    font-weight:bold;
+}
+
+.contenedor-icono svg{
+    height:75px;
+    width:65px;
+}
+
+.detalles{
+    text-decoration:none;
+}
+
+.raza1{
+    display:none;
+}
+
+/*login del crud*/ 
+
+.fondo{
+    background-image: url(../recursos/fondoveterinaria2.jpg);
+    background-size: cover;
+}
+
+.bgcard{
+    border-radius:10px !important;
+    background-color:#33B5FF !important;
+    padding: 25px 5px;
+   
+}
+
+.usuario-img{
+    opacity:100%;
+}
+
+.imguser{
+    width: 100px;
+    height: 100px;
+}
+
+
 
